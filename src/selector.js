53b2420a3a47b8803e20cd0d7c6db1d13012c3da
JackPadalino <pada0867@gmail.com>
2022-08-29 20:47:12 -0400

    added notes

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,14 @@
 // YOUR CODE HERE
+
+// Laptop class
+/*
+class Laptop{
+    constructor(year,hd){
+        this.year=year;
+        this.hd=hd;
+    };
+    checkSpecs(){
+        return `Year: ${this.year}, HD: ${this.hd}`;
+    };
+};
+*/
